039655739e9810172827545f28028355e3d31b19
Samuel Shear <dspfgh34@gmail.com>
2021-08-07 18:20:51 -0400

    Added http operations to main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from database import * 
 
 origins = ["*"] # This will eventually be changed to only the origins you will use once it's deployed, to secure the app a bit more.
 
@@ -13,6 +15,36 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get('/')
+@app.get("/")
 def get_root():
     return {"Ping": "Pong"}
+
+@app.get("/api/get-todo/{nanoid}", response_model=Todo)
+async def get_one_todo(nanoid):
+    todo = await fetch_one_todo(nanoid)
+    if not todo: raise HTTPException(404)
+    return todo
+
+@app.get("/api/get-todo", response_model=Todo)
+async def get_todos():
+    todos = await fetch_all_todos()
+    if not todos: raise HTTPException(404)
+    return todos
+
+@app.post("/api/add-todo", response_model=Todo)
+async def add_todo(todo: Todo):
+    result = await create_todo(todo)
+    if not result: raise HTTPException(400)
+    return result
+
+@app.put("/api/update-todo/{nanoid}", response_model=Todo)
+async def update_todo(nanoid, title, desc, checked):
+    result = await change_todo(nanoid, title, desc, checked)
+    if not result: raise HTTPException(400)
+    return result
+
+@app.delete("/api/delete-todo/{nanoid}")
+async def delete_todo(nanoid):
+    result = await remove_todo(nanoid)
+    if not result: raise HTTPException(400)
+    return result
